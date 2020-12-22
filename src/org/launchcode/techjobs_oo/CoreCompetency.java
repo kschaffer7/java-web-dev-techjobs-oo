fc2a7185cd17b54dbb68b5d45039344d964fc62c
Kirsti Schaffer <kirstischaffer@gmail.com>
2020-12-21 19:52:02 -0600

    steps 1 and 2 completed

--- src/org/launchcode/techjobs_oo/CoreCompetency.java
+++ src/org/launchcode/techjobs_oo/CoreCompetency.java
@@ -32,11 +32,26 @@ public class CoreCompetency {
         return id == that.id;
     }
 
+
+
     @Override
     public int hashCode() {
         return Objects.hash(id);
     }
 
-    // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
+
+// TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
